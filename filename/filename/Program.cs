79172fc2fb0ee1302962e0da4c37f76f0390a3a7
Дмитрий Кассин <kassindima1@gmail.com>
2022-07-21 21:38:18 +0300

    .........

--- filename/filename/Program.cs
+++ filename/filename/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//string directory = "D:\\Programs\\ Загрузки"; //Директория
+//String[] dirs = Directory.GetFiles(directory, ".txt"); //Поиск всех файлов в папке с расширением txt
+
+using Microsoft.Win32;
+
+
+//SOFTWARE\Microsoft\Windows\CurrentVersion\Run
+//RegistryKey registry = Registry.CurrentUser;
+//RegistryKey myAppKey = Registry.OpenSubKey(@"SOFTWARE\\Microsoft\Windows\CurrentVersion\Run", true);
+//myAppKey.SetValue("MyApp", $"{Directory.GetCurrentDirectory()}//ConsoleApp3.exe");
+
+//SOFTWARE\Microsoft\Windows\CurrentVersion\Run
+RegistryKey registry = Registry.LocalMachine;
+RegistryKey myAppKey = registry.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+myAppKey.SetValue("MyApp", $"{Directory.GetCurrentDirectory()}//DIMKA.exe");
+
