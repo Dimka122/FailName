cf92b1b8a65b43e41698f4b4ca4760033582d673
Дмитрий Кассин <kassindima1@gmail.com>
2022-07-22 18:57:20 +0300

    /////////

--- filename/filename/Program.cs
+++ filename/filename/Program.cs
@@ -2,7 +2,7 @@
 //String[] dirs = Directory.GetFiles(directory, ".txt"); //Поиск всех файлов в папке с расширением txt
 
 using Microsoft.Win32;
-using System.IO;
+using System.Text;
 
 
 //SOFTWARE\Microsoft\Windows\CurrentVersion\Run
@@ -10,33 +10,17 @@ using System.IO;
 //RegistryKey myAppKey = Registry.OpenSubKey(@"SOFTWARE\\Microsoft\Windows\CurrentVersion\Run", true);
 //myAppKey.SetValue("MyApp", $"{Directory.GetCurrentDirectory()}//ConsoleApp3.exe");
 
-//SOFTWARE\Microsoft\Windows\CurrentVersion\Run
-RegistryKey registry = Registry.LocalMachine;
-RegistryKey myAppKey = registry.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
-myAppKey.SetValue("filename", $"{Directory.GetCurrentDirectory()}//filename.exe");
 
-string dirName = "C:\\Users\\Дима\\Изображения";
-var directory = Directory.GetFiles(dirName, ".txt");
+//RegistryKey registry = Registry.LocalMachine;
+//RegistryKey myAppKey = registry.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+//myAppKey.SetValue("filename", $"{Directory.GetCurrentDirectory()}//filename.exe");
 
-if (directory.Exists)
-{
-    Console.WriteLine("Подкаталоги");
-    DirectoryInfo[] dirs = directory.GetDirectories();
-}
-foreach (DirectoryInfo dir in dirs)
-{
-    Console.WriteLine(dir.FullName);
-}
+string path = "/Users/eugene/Documents/content.txt";
 
-    Console.WriteLine();
-Console.WriteLine("Файлы");
-FileInfo[] files = directory.GetFiles();
-foreach(FileInfo file in files)
-{
-    Console.WriteLine(files.Fullname);
-}
+string origanalText = "Привет Дима! Как дела?";
 
+await File.WriteAllTextAsync(path, origanalText, Encoding.Unicode);
+await File.AppendAllTextAsync(path, "\nПривет. Хорошо!", Encoding.Unicode);
 
-
-
-
+string fileText = await File.ReadAllTextAsync(path, Encoding.Unicode);
+Console.WriteLine(fileText);
