b06ce8c91bfd3cddaeb0650e6f4dd71a41286e19
Дмитрий Кассин <kassindima1@gmail.com>
2022-07-24 06:47:39 +0300

    \//\\/\

--- filename/filename/Program.cs
+++ filename/filename/Program.cs
@@ -30,23 +30,17 @@ string fileText = await File.ReadAllTextAsync(path, Encoding.Unicode);
 Console.WriteLine(fileText);
 
 string ShortCutPath = Environment.CurrentDirectory + @"C:\Code\FileName\fileName\filename.exe"; // полный путь, где будет создан ярлык
-// Создаем объект ярлыка
 
-
-// Добавляем запись в реестр
 Microsoft.Win32.RegistryKey Key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\", true);
 Key.SetValue("filename", ShortCutPath);
 
-//Microsoft.Win32.RegistryKey Key =
-//        Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run\\", true);
-//Key.SetValue("filename", $"{Directory.GetCurrentDirectory()}//filename.exe");
-//Key.Close();
+
 
 //удаляем  
-//#pragma warning disable CA1416 // Проверка совместимости платформы
+
 //Microsoft.Win32.RegistryKey key =
-//Microsoft.Win32.Registry.LocalMachine.OpenSubKey(
+//Microsoft.Win32.Registry.CurrentUser.OpenSubKey(
 //    "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\", true);
-//#pragma warning restore CA1416 // Проверка совместимости платформы
-//key.DeleteValue("Dimka", false);
+
+//key.DeleteValue("filename", false);
 //key.Close();
