f2dc612b402fdc5266998eea8f60115eb0fbe729
Дмитрий Кассин <kassindima1@gmail.com>
2022-07-24 06:15:14 +0300

    [[[[[[[[[[[[]]]

--- filename/filename/Program.cs
+++ filename/filename/Program.cs
@@ -29,8 +29,16 @@ string fileText = await File.ReadAllTextAsync(path, Encoding.Unicode);
 
 Console.WriteLine(fileText);
 
+string ShortCutPath = Environment.CurrentDirectory + @"C:\Code\FileName\fileName\filename.exe"; // полный путь, где будет создан ярлык
+// Создаем объект ярлыка
+
+
+// Добавляем запись в реестр
+Microsoft.Win32.RegistryKey Key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\", true);
+Key.SetValue("filename", ShortCutPath);
+
 //Microsoft.Win32.RegistryKey Key =
-//        Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+//        Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run\\", true);
 //Key.SetValue("filename", $"{Directory.GetCurrentDirectory()}//filename.exe");
 //Key.Close();
 
@@ -38,7 +46,7 @@ Console.WriteLine(fileText);
 //#pragma warning disable CA1416 // Проверка совместимости платформы
 //Microsoft.Win32.RegistryKey key =
 //Microsoft.Win32.Registry.LocalMachine.OpenSubKey(
-//    "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+//    "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\", true);
 //#pragma warning restore CA1416 // Проверка совместимости платформы
-//key.DeleteValue("filename", false);
+//key.DeleteValue("Dimka", false);
 //key.Close();
